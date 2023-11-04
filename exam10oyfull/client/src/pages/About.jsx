4923b95c2efe487d8ceaada1100f6c1eda6c8e7c
Firdavs <119524015+nazarovfirdavs@users.noreply.github.com>
2023-11-04 21:47:24 +0500

    Add files via upload

---
+++ exam10oyfull/client/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="bg-slate-300">
+      <img
+        src="https://stimg.cardekho.com/images/carexteriorimages/930x620/BMW/X7/10571/1689673096346/front-left-side-47.jpg"
+        alt=""
+      />
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Firdavs's market
+      </h1>
+      <p className="mb-4 text-slate-700">
+        If you like this site, here is my number:
+      </p>
+      <p className="mb-4 text-slate-700">+99897-543-83-05</p>
+    </div>
+  );
+}
